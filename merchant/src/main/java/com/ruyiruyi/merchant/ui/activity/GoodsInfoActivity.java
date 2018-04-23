4d8127cc4cbd70d46915b4b6253ad763b33622bd
dongbin <1349975620@qq.com>
2018-04-23 15:56:11 +0800

    merchant

---
+++ merchant/src/main/java/com/ruyiruyi/merchant/ui/activity/GoodsInfoActivity.java
@@ -0,0 +1,15 @@
+package com.ruyiruyi.merchant.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ruyiruyi.merchant.R;
+
+public class GoodsInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_info);
+    }
+}
