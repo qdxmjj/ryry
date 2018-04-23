be479735008a151d59e14893ae0c52641b0c98df
geyang <614009510@qq.com>
2018-04-23 15:47:24 +0800

    查询提交

---
+++ rylibrary/src/main/java/com/ruyiruyi/rylibrary/android/searchview/ICallBack.java
@@ -0,0 +1,11 @@
+package com.ruyiruyi.rylibrary.android.searchview;
+
+/**
+ * Created by Carson_Ho on 17/8/10.
+ */
+
+public interface ICallBack {
+    void SearchAciton(String string);
+
+
+}
---
+++ rylibrary/src/main/java/com/ruyiruyi/rylibrary/android/searchview/ItemCallBack.java
@@ -0,0 +1,9 @@
+package com.ruyiruyi.rylibrary.android.searchview;
+
+/**
+ * Created by Carson_Ho on 17/8/11.
+ */
+
+public interface ItemCallBack {
+    void ItemClick(String string);
+}
