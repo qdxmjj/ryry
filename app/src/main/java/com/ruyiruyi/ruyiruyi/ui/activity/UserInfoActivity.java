032bb234ba9505b86b9aead6218b0cac72bc1ae7
dongbin <1349975620@qq.com>
2018-05-16 10:43:41 +0800

    个人信息 用户版

--- app/src/main/java/com/ruyiruyi/ruyiruyi/ui/activity/UserInfoActivity.java
+++ app/src/main/java/com/ruyiruyi/ruyiruyi/ui/activity/UserInfoActivity.java
@@ -1,19 +1,74 @@
 package com.ruyiruyi.ruyiruyi.ui.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.ruyiruyi.ruyiruyi.R;
+import com.ruyiruyi.rylibrary.android.rx.rxbinding.RxViewAction;
 import com.ruyiruyi.rylibrary.base.BaseActivity;
 import com.ruyiruyi.rylibrary.cell.ActionBar;
+import com.ruyiruyi.rylibrary.image.ImageUtils;
+import com.ruyiruyi.rylibrary.utils.glide.GlideCircleTransform;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import rx.functions.Action1;
 
 public class UserInfoActivity extends BaseActivity {
 
     private ActionBar mActionBar;
+    private LinearLayout ll_change;
+    private LinearLayout ll_sex;
+    private LinearLayout ll_email;
+    private LinearLayout ll_birth;
+    private ImageView change_img;
+    private EditText user_name;
+    private TextView user_sex;
+    private TextView user_email;
+    private TextView user_birth;
+    private TextView tv_save_;
+
+    private final int CHOOSE_PICTURE = 0;
+    private final int TAKE_PICTURE = 1;
+    private Uri tempUri;
+    private Bitmap imgBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_user_info);
         mActionBar = (ActionBar) findViewById(R.id.acbar_info);
         mActionBar.setTitle("个人信息");
@@ -28,6 +83,202 @@ public class UserInfoActivity extends BaseActivity {
             }
         });
 
+        initView();
+        bindView();
+
+
+    }
+
+    private void bindView() {
+        RxViewAction.clickNoDouble(ll_change).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                showPicInputDialog();
+            }
+        });
+        RxViewAction.clickNoDouble(tv_save_).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                //条件判断操作
+
+
+                //判断通过
+                showSaveDialog("确定保存修改吗");
+            }
+        });
+    }
+
+    private void showPicInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserInfoActivity.this);
+        builder.setTitle("上传照片");
+        String[] items = {"选择本地照片", "拍照"};
+        builder.setNegativeButton("取消", null);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case CHOOSE_PICTURE://选择本地照片
+                        Intent openBendiPicIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        openBendiPicIntent.setType("image/*");
+                        startActivityForResult(openBendiPicIntent, CHOOSE_PICTURE);
+                        break;
+                    case TAKE_PICTURE://拍照
+                        takePicture();
+                        break;
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    private void takePicture() {
+        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (Build.VERSION.SDK_INT >= 23) {
+            // 需要申请动态权限
+            int check = ContextCompat.checkSelfPermission(UserInfoActivity.this, permissions[0]);
+            // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+            if (check != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+        Intent openCameraIntent = new Intent(
+                MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = null;
+        file = new File(Environment
+                .getExternalStorageDirectory(), "userinfoimg.jpg");
+
+        //判断是否是AndroidN以及更高的版本
+        if (Build.VERSION.SDK_INT >= 24) {
+            openCameraIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            tempUri = FileProvider.getUriForFile(UserInfoActivity.this, "com.ruyiruyi.merchant.fileProvider", file);
+        } else {
+            tempUri = Uri.fromFile(new File(Environment
+                    .getExternalStorageDirectory(), "image.jpg"));
+        }
+        // 指定照片保存路径（SD卡），image.jpg为一个临时文件，每次拍照后这个图片都会被替换
+        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
+        startActivityForResult(openCameraIntent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CHOOSE_PICTURE:
+                    Uri uri = data.getData();
+                    setImageToViewFromPhone(uri);
+                    break;
+                case TAKE_PICTURE:
+                    setImageToViewFromPhone(tempUri);
+                    break;
+
+            }
+        }
+    }
+
+    //未剪辑照片
+    private void setImageToViewFromPhone(Uri uri) {
+        int degree = ImageUtils.readPictureDegree(uri.toString());
+        if (uri != null) {
+            Bitmap photo = null;
+            try {
+                photo = ImageUtils.getBitmapFormUri(UserInfoActivity.this, uri);
+            } catch (IOException e) {
+            }
+            imgBitmap = rotaingImageView(degree, photo);
+//   2          mGoodsImg.setImageBitmap(imgBitmap);
+            //Glide 加载BitMap需要先将bitmap对象转换为字节,在加载;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            imgBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] bytes = baos.toByteArray();
+            Glide.with(this).load(bytes)
+                    .transform(new GlideCircleTransform(UserInfoActivity.this))
+                    .into(change_img);
+        }
+    }
+
+    public static Bitmap rotaingImageView(int angle, Bitmap bitmap) {
+        Bitmap returnBm = null;
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            returnBm = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        } catch (OutOfMemoryError e) {
+        }
+        if (returnBm == null) {
+            returnBm = bitmap;
+        }
+        if (bitmap != returnBm) {
+            bitmap.recycle();
+        }
+        return returnBm;
+    }
+
+
+    private void initView() {
+        ll_change = findViewById(R.id.ll_change);
+        ll_sex = findViewById(R.id.ll_sex);
+        ll_email = findViewById(R.id.ll_email);
+        ll_birth = findViewById(R.id.ll_birth);
+        change_img = findViewById(R.id.change_img);
+        user_name = findViewById(R.id.user_name);
+        user_sex = findViewById(R.id.user_sex);
+        user_email = findViewById(R.id.user_email);
+        user_birth = findViewById(R.id.user_birth);
+        tv_save_ = findViewById(R.id.tv_save_);
+
+    }
+
+
+    private void showDialog(String error) {
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_error, null);
+        TextView error_text = (TextView) dialogView.findViewById(R.id.error_text);
+        error_text.setText(error);
+        dialog.setTitle("如意如驿");
+        dialog.setIcon(R.drawable.ic_logo_login);
+        dialog.setView(dialogView);
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE, "确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+        //设置按钮颜色
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.theme_primary));
+    }
+
+    private void showSaveDialog(String error) {
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_save_commit, null);
+        TextView error_text = (TextView) dialogView.findViewById(R.id.save_text);
+        error_text.setText(error);
+        dialog.setTitle("如意如驿");
+        dialog.setIcon(R.drawable.ic_logo_login);
+        dialog.setView(dialogView);
+        dialog.setButton(DialogInterface.BUTTON_NEGATIVE, "再看看", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        //确认提交 请求提交数据
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE, "是的", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        dialog.show();
+        //设置按钮颜色
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.theme_primary));
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.theme_primary));
 
     }
 }
