d5ecf9b73c6d0a5cf5a763da62482caa914cb127
dongbin <1349975620@qq.com>
2018-08-10 09:23:20 +0800

    分享邀请状态判断 PromotionActivity

--- app/src/main/java/com/ruyiruyi/ruyiruyi/ui/activity/PromotionActivity.java
+++ app/src/main/java/com/ruyiruyi/ruyiruyi/ui/activity/PromotionActivity.java
@@ -143,7 +143,18 @@ public class PromotionActivity extends RyBaseActivity implements PromotionViewBi
                         for (int i = 0; i < shareRelationList.length(); i++) {
                             JSONObject bean = (JSONObject) shareRelationList.get(i);
                             PromotionHasperson hasperson = new PromotionHasperson();
-                            hasperson.setUserState("已邀请");
+                            int promostatus = bean.getInt("status");
+                            switch (promostatus) {
+                                case 1:
+                                    hasperson.setUserState("已邀请");
+                                    break;
+                                case 2:
+                                    hasperson.setUserState("已注册App");
+                                    break;
+                                case 3:
+                                    hasperson.setUserState("已注册车辆信息");
+                                    break;
+                            }
                             long createdTime = bean.getLong("createdTime");
                             String s = new UtilsRY().getTimestampToString(createdTime);
                             hasperson.setUserTime(s);
